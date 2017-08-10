1a83e3c62e4ce1e114fd44c2e61a5949fdf557ac
Defn Nghiem <iam@defn.sh>
2017-08-10 00:56:16 -0700

    private ivp6v can exist without needing nat

--- main.tf
+++ main.tf
@@ -124,7 +124,7 @@ resource "aws_route" "service_v6" {
   route_table_id              = "${element(aws_route_table.service.*.id,count.index)}"
   destination_ipv6_cidr_block = "::/0"
   egress_only_gateway_id      = "${data.terraform_remote_state.env.egw_gateway}"
-  count                       = "${var.want_ipv6*var.want_nat*var.az_count*(signum(var.public_network)-1)*-1}"
+  count                       = "${var.az_count*(signum(var.public_network)-1)*-1}"
 }
 
 resource "aws_route_table_association" "service" {
@@ -163,8 +163,8 @@ resource "aws_route" "service_public" {
 resource "aws_route" "service_public_v6" {
   route_table_id              = "${element(aws_route_table.service_public.*.id,count.index)}"
   destination_ipv6_cidr_block = "::/0"
-  egress_only_gateway_id      = "${data.terraform_remote_state.env.egw_id}"
-  count                       = "${var.want_ipv6*var.want_nat*var.az_count*(signum(var.public_network)-1)*-1}"
+  gateway_id                  = "${data.terraform_remote_state.env.igw_id}"
+  count                       = "${var.az_count*signum(var.public_network)}"
 }
 
 resource "aws_route_table_association" "service_public" {
