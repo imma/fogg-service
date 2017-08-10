e5260176d60b000c49f188e366d3e76d86959be7
Defn Nghiem <iam@defn.sh>
2017-08-10 01:39:40 -0700

    fix ternary on ipv6

--- main.tf
+++ main.tf
@@ -82,8 +82,8 @@ resource "aws_subnet" "service" {
   cidr_block              = "${cidrsubnet(data.aws_vpc.current.cidr_block,var.service_bits,element(concat(split(" ",lookup(data.terraform_remote_state.org.org,"service_${data.terraform_remote_state.app.app_name}_${var.service_name}","")),split(" ",lookup(data.terraform_remote_state.org.org,"service_${var.service_name}",""))),count.index))}"
   map_public_ip_on_launch = "${signum(var.public_network) == 1 ? "true" : "false"}"
 
-  ipv6_cidr_block                 = "${var.want_ipv6 ? cidrsubnet(data.aws_vpc.current.ipv6_cidr_block,8,element(concat(split(" ",lookup(data.terraform_remote_state.org.org,"service_v6_${data.terraform_remote_state.app.app_name}_${var.service_name}","")),split(" ",lookup(data.terraform_remote_state.org.org,"service_v6_${var.service_name}",""))),count.index)) : ""}"
-  assign_ipv6_address_on_creation = "${var.want_ipv6 ? "true" : "false"}"
+  ipv6_cidr_block                 = "${var.want_ipv6 == "1" ? cidrsubnet(data.aws_vpc.current.ipv6_cidr_block,8,element(concat(split(" ",lookup(data.terraform_remote_state.org.org,"service_v6_${data.terraform_remote_state.app.app_name}_${var.service_name}","")),split(" ",lookup(data.terraform_remote_state.org.org,"service_v6_${var.service_name}",""))),count.index)) : "0"}"
+  assign_ipv6_address_on_creation = "${var.want_ipv6 == "1" ? "true" : "false"}"
 
   count = "${var.az_count}"
 
