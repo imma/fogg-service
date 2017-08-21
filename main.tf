e3a0c5313460e8f7ba4b199d395cc3843b62a5df
Defn Nghiem <iam@defn.sh>
2017-08-21 08:36:39 +0000

    create a storage device per service

--- main.tf
+++ main.tf
@@ -892,11 +892,20 @@ resource "packet_project" "service" {
 resource "packet_device" "service" {
   hostname         = "packet-${data.terraform_remote_state.app.app_name}${var.service_default == "1" ? "" : "-${var.service_name}"}${count.index+1}.${data.terraform_remote_state.env.private_zone_name}"
   project_id       = "${packet_project.service.id}"
-  billing_cycle    = "hourly"
-  operating_system = "${var.packet_operating_system}"
   facility         = "${var.packet_facility}"
   plan             = "${var.packet_plan}"
+  billing_cycle    = "hourly"
+  operating_system = "${var.packet_operating_system}"
   count            = "${var.want_packet*var.packet_instance_count}"
+}
+
+resource "packet_volume" "service" {
+  project_id   = "${packet_project.service.id}"
+  facility     = "${var.packet_facility}"
+  plan         = "storage_1"
+  biling_cycle = "hourly"
+  size         = "40"
+  count        = "${var.want_packet}"
 }
 
 resource "aws_route53_record" "packet_instance" {
