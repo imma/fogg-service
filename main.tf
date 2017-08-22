ba5aeac16b7c2cc8e4fd1e6b278c121da23ca76d
Defn Nghiem <iam@defn.sh>
2017-08-22 11:10:01 +0000

    use invoke_arn

--- main.tf
+++ main.tf
@@ -1019,10 +1019,10 @@ resource "aws_api_gateway_integration" "status" {
   rest_api_id             = "${aws_api_gateway_rest_api.service.id}"
   resource_id             = "${aws_api_gateway_resource.service.id}"
   http_method             = "${aws_api_gateway_method.service.http_method}"
+  uri                     = "${aws_lambda_function.status.invoke_arn}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   content_handling        = "CONVERT_TO_TEXT"
-  uri                     = "arn:aws:apigateway:${var.env_region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.env_region}:${data.terraform_remote_state.org.aws_account_id}:function:${aws_lambda_function.status.function_name}/invocations"
 }
 
 resource "aws_api_gateway_deployment" "status_staging" {
