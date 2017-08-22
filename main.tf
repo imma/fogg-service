e5dcc366051159be84201c337189f9090c299ffe
Defn Nghiem <iam@defn.sh>
2017-08-22 05:02:59 +0000

    trying to get kms changes to converge in one passs

--- main.tf
+++ main.tf
@@ -877,7 +877,7 @@ resource "aws_kms_key" "service" {
 
 resource "aws_kms_alias" "service" {
   name          = "alias/${data.terraform_remote_state.env.env_name}-${data.terraform_remote_state.app.app_name}-${var.service_name}"
-  target_key_id = "${element(coalescelist(aws_kms_key.service.*.key_id,list(data.terraform_remote_state.env.kms_key_id)),0)}"
+  target_key_id = "${var.want_kms ? join(" ",aws_kms_key.service.*.key_id) : data.terraform_remote_state.env.kms_key_id}"
 }
 
 resource "aws_codecommit_repository" "service" {
