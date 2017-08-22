0b9bf75c095da06b81b504c29e484714dc9e5f45
Defn Nghiem <iam@defn.sh>
2017-08-22 07:55:45 +0000

    lambdas live in common subnet with the service sgs

--- main.tf
+++ main.tf
@@ -1001,7 +1001,7 @@ resource "aws_lambda_function" "status" {
   publish          = true
 
   vpc_config {
-    subnet_ids         = ["${compact(concat(aws_subnet.service.*.id,aws_subnet.service_v6.*.id,formatlist(var.want_subnets ? "%[3]s" : (var.public_network ? "%[1]s" : "%[2]s"),data.terraform_remote_state.env.public_subnets,data.terraform_remote_state.env.common_subnets,data.terraform_remote_state.env.fake_subnets)))}"]
+    subnet_ids         = ["${data.terraform_remote_state.env.common_subnets}"]
     security_group_ids = ["${concat(list(data.terraform_remote_state.env.sg_env,signum(var.public_network) == 1 ?  data.terraform_remote_state.env.sg_env_public : data.terraform_remote_state.env.sg_env_private,aws_security_group.service.id),list(data.terraform_remote_state.app.app_sg))}"]
   }
 }
