cf871fd7b440fed60ff0fd1c2ff27304e5cb64d7
Defn Nghiem <iam@defn.sh>
2017-08-25 23:45:08 +0000

    route to vpn

--- main.tf
+++ main.tf
@@ -154,6 +154,13 @@ resource "aws_route" "service_nat" {
   count                  = "${var.want_subnets*var.want_nat_instance*var.az_count*(signum(var.public_network)-1)*-1}"
 }
 
+resource "aws_route" "service_vpn" {
+  route_table_id         = "${element(aws_route_table.service.*.id,count.index)}"
+  destination_cidr_block = "10.8.0.0/0"
+  instance_id            = "${element(data.terraform_remote_state.env.vpn_instances,count.index)}"
+  count                  = "${var.want_subnets*var.want_vpn*var.az_count}"
+}
+
 resource "aws_route" "service_v6" {
   route_table_id              = "${element(aws_route_table.service.*.id,count.index)}"
   destination_ipv6_cidr_block = "::/0"
