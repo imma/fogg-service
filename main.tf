18764dfdb9e167912880c284ff9bd4cb7efa5e37
Defn Nghiem <iam@defn.sh>
2017-08-25 22:15:24 -0700

    add vpn route

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ resource "aws_route" "service_vpn" {
   route_table_id         = "${element(aws_route_table.service.*.id,count.index)}"
   destination_cidr_block = "10.8.0.0/24"
   instance_id            = "${element(data.terraform_remote_state.env.vpn_instances,count.index)}"
-  count                  = "${var.want_subnets*var.want_nat*var.want_vpn*var.az_count}"
+  count                  = "${var.want_subnets*var.want_vpn*var.az_count*(signum(var.public_network)-1)*-1}"
 }
 
 resource "aws_route" "service_v6" {
