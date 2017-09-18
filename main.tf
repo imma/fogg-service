7f3209afff19b7d090efd5bac7cd6a8d78ff4bc8
Defn Nghiem <iam@defn.sh>
2017-09-17 20:31:15 -0700

    no more app name cmbinations

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_subnet" "service" {
 
   availability_zone = "${element(data.aws_availability_zones.azs.names,count.index)}"
 
-  cidr_block              = "${cidrsubnet(data.aws_vpc.current.cidr_block,var.service_bits,element(concat(split(" ",lookup(var.service,"${data.terraform_remote_state.app.app_name}_${var.service_name}","")),split(" ",lookup(var.service,var.service_name,""))),count.index))}"
+  cidr_block              = "${cidrsubnet(data.aws_vpc.current.cidr_block,var.service_bits,element(split(" ",lookup(var.service,var.service_name,"")),count.index))}"
   map_public_ip_on_launch = "${signum(var.public_network) == 1 ? "true" : "false"}"
 
   count = "${var.want_subnets*var.az_count*(var.want_ipv6 - 1)*-1}"
@@ -102,7 +102,7 @@ resource "aws_subnet" "service_v6" {
 
   availability_zone = "${element(data.aws_availability_zones.azs.names,count.index)}"
 
-  cidr_block              = "${cidrsubnet(data.aws_vpc.current.cidr_block,var.service_bits,element(concat(split(" ",lookup(var.service,"${data.terraform_remote_state.app.app_name}_${var.service_name}","")),split(" ",lookup(var.service,var.service_name,""))),count.index))}"
+  cidr_block              = "${cidrsubnet(data.aws_vpc.current.cidr_block,var.service_bits,element(split(" ",lookup(var.service,var.service_name,"")),count.index))}"
   map_public_ip_on_launch = "${signum(var.public_network) == 1 ? "true" : "false"}"
 
   count = "${var.want_subnets*var.az_count*var.want_ipv6}"
