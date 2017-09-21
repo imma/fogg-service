05d10ecc3fdf7e1398618f2626ced9d1a9f5a71e
Defn Nghiem <iam@defn.sh>
2017-09-21 00:14:24 -0700

    no more private/public network

--- main.tf
+++ main.tf
@@ -121,11 +121,9 @@ resource "aws_subnet" "service_v6" {
 }
 
 resource "aws_network_interface" "service" {
-  subnet_id = "${element(compact(concat(aws_subnet.service.*.id,aws_subnet.service_v6.*.id)),count.index)}"
-
-  #private_ips = ["${cidrhost(element(compact(concat(aws_subnet.service.*.cidr_block,aws_subnet.service_v6.*.cidr_block)),count.index),-2)}"]
-  private_ips_count = 1
-  count             = "${var.want_subnets*var.az_count*var.want_subnets}"
+  subnet_id       = "${element(compact(concat(aws_subnet.service.*.id,aws_subnet.service_v6.*.id)),count.index)}"
+  security_groups = ["${data.terraform_remote_state.env.sg_env}", "${data.terraform_remote_state.app.app_sg}", "${aws_security_group.service.id}"]
+  count           = "${var.want_subnets*var.az_count*var.want_subnets}"
 
   tags {
     "Name"      = "${data.terraform_remote_state.env.env_name}-${data.terraform_remote_state.app.app_name}-${var.service_name}"
@@ -134,36 +132,6 @@ resource "aws_network_interface" "service" {
     "Service"   = "${var.service_name}"
     "ManagedBy" = "terraform"
   }
-}
-
-resource "aws_network_interface_sg_attachment" "env" {
-  security_group_id    = "${data.terraform_remote_state.env.sg_env}"
-  network_interface_id = "${element(aws_network_interface.service.*.id,count.index)}"
-  count                = "${var.want_subnets*var.az_count*var.want_subnets}"
-}
-
-resource "aws_network_interface_sg_attachment" "env_private" {
-  security_group_id    = "${data.terraform_remote_state.env.sg_env_private}"
-  network_interface_id = "${element(aws_network_interface.service.*.id,count.index)}"
-  count                = "${var.want_subnets*var.az_count*var.want_subnets*signum(var.public_network - 1)*-1}"
-}
-
-resource "aws_network_interface_sg_attachment" "env_public" {
-  security_group_id    = "${data.terraform_remote_state.env.sg_env_public}"
-  network_interface_id = "${element(aws_network_interface.service.*.id,count.index)}"
-  count                = "${var.want_subnets*var.az_count*var.want_subnets*signum(var.public_network)}"
-}
-
-resource "aws_network_interface_sg_attachment" "app" {
-  security_group_id    = "${data.terraform_remote_state.app.app_sg}"
-  network_interface_id = "${element(aws_network_interface.service.*.id,count.index)}"
-  count                = "${var.want_subnets*var.az_count*var.want_subnets}"
-}
-
-resource "aws_network_interface_sg_attachment" "service" {
-  security_group_id    = "${aws_security_group.service.id}"
-  network_interface_id = "${element(aws_network_interface.service.*.id,count.index)}"
-  count                = "${var.want_subnets*var.az_count*var.want_subnets}"
 }
 
 resource "aws_route_table" "service" {
