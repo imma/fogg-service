ec2e166748e87c0e855af235fd1818ff9413c1f6
Defn Nghiem <iam@defn.sh>
2017-10-09 18:22:23 -0700

    timestamp function

--- main.tf
+++ main.tf
@@ -824,7 +824,7 @@ resource "aws_elasticache_cluster" "service" {
 }
 
 resource "aws_elasticache_parameter_group" "service" {
-  name = "${data.terraform_remote_state.env.env_name}-${data.terraform_remote_state.app.app_name}-${var.service_name}-${timestamp}"
+  name = "${data.terraform_remote_state.env.env_name}-${data.terraform_remote_state.app.app_name}-${var.service_name}-${timestamp()}"
 
   family = "redis3.2"
 
